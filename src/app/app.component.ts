ebe9d4fc612e816525f1f3fdf0882d4ef8c74a99
patshone96 <patrick_oliver_@hotmail.es>
2022-07-15 19:55:15 +0200

    MiniCalculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiniCalculadoraApp';
+  title = 'Mini Calculadora';
+
+  numero1:string = "0";
+
+  numero2:string = "0";
+
+  resultado:number = 0;
+
+  sumar():void{
+
+
+    this.resultado = parseInt(this.numero1)+ parseInt(this.numero2); 
+
+
+  }
+
+
+  restar():void{
+
+
+    this.resultado = parseInt(this.numero1) - parseInt(this.numero2); 
+
+
+  }
+
+  multiplicar():void{
+
+
+    this.resultado = parseInt(this.numero1) * parseInt(this.numero2); 
+
+
+  }
+
+  dividir():void{
+
+
+    this.resultado = parseInt(this.numero1) / parseInt(this.numero2); 
+
+
+  }
 }
